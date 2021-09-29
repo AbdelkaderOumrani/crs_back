b70ceed22ae8014f3d9cb982e57b3b2ab235758d
AbdelkaderOumrani <kadi.blaugrana@gmail.com>
2021-09-29 06:20:22 +0100

    mock data

--- src/routes/generator.ts
+++ src/routes/generator.ts
@@ -1,176 +1,98 @@
-import express, { Request, Response } from "express";
+import { IFormationCourseDocument } from './../models/formationCourse';
+import { DB_USERNAME } from './../dbConfig';
+import express, { Request, Response } from 'express';
 const router = express.Router();
-import { Specialite } from "../models/specialite";
-import { User } from "../models/user";
-import { Course } from "../models/course";
-import { CourseHistory } from "../models/courseHistory";
-import { generateNumber } from "../helpers";
-router.get("/generateHistory", [], async (req: Request, res: Response) => {
-  const students = await User.find();
-  const courses = await Course.find();
+import { Specialite } from '../models/specialite';
+import { User } from '../models/user';
+import { Course } from '../models/course';
+import { CourseHistory } from '../models/courseHistory';
+import { generateArray, generateNumber } from '../helpers';
+import { Skill } from '../models/skill';
+import { dataSkills, formationsData } from '../mockdata/data';
+import { Formation } from '../models/formation';
+import { FormationCourse } from '../models/formationCourse';
 
-  const result: any = [];
-
-  const studentsWithCourses = students.map((student) => {
-    const optCourses = courses.filter(
-      (c) => c.specialite === student.specialite && c.optional
-    );
-    const obgCourses = courses.filter(
-      (c) => c.specialite === student.specialite && !c.optional
-    );
-    return {
-      studentId: student.id,
-      optCourses,
-      obgCourses,
-    };
+router.get('/generateSkills', [], async (req: Request, res: Response) => {
+  // const skills: string[] = req.body.skills;
+  console.log(`generator`);
+  dataSkills.map(async (sk) => {
+    await Skill.build({ name: sk, courses: [] }).save();
   });
-
-  studentsWithCourses.map(async (row, i) => {
-    console.log(`i`, i);
-    //Obligatoire
-    row.obgCourses.map(async (crs) => {
-      await CourseHistory.build({
-        student: row.studentId,
-        course: crs.id,
-        term: crs.term,
-        hours: crs.hours,
-        note: crs.hours > 30 ? generateNumber(6, 15) : generateNumber(6, 20),
-      }).save();
-    });
-
-    //OPtional
-    const termOne = row.optCourses.filter((crs) => crs.term === 1);
-    const termTwo = row.optCourses.filter((crs) => crs.term === 2);
-    const termThree = row.optCourses.filter((crs) => crs.term === 3);
-    const termFour = row.optCourses.filter((crs) => crs.term === 4);
-
-    const randomFromOne = getRandom(termOne, 4);
-    const randomFromTwo = getRandom(termTwo, 4);
-    const randomFromThree = getRandom(termThree, 4);
-    const randomFromFour = getRandom(termFour, 4);
-    console.log(`randomFromOne.length`, randomFromOne.length);
-
-    randomFromOne.map(async (crs) => {
-      await CourseHistory.build({
-        student: row.studentId,
-        course: crs.id,
-        term: crs.term,
-        hours: crs.hours,
-        note: crs.hours > 30 ? generateNumber(6, 15) : generateNumber(6, 20),
-      }).save();
-    });
-    randomFromTwo.map(async (crs) => {
-      await CourseHistory.build({
-        student: row.studentId,
-        course: crs.id,
-        term: crs.term,
-        hours: crs.hours,
-        note: crs.hours > 30 ? generateNumber(6, 15) : generateNumber(6, 20),
-      }).save();
-    });
-    randomFromThree.map(async (crs) => {
-      await CourseHistory.build({
-        student: row.studentId,
-        course: crs.id,
-        term: crs.term,
-        hours: crs.hours,
-        note: crs.hours > 30 ? generateNumber(6, 15) : generateNumber(6, 20),
-      }).save();
-    });
-    randomFromFour.map(async (crs) => {
-      await CourseHistory.build({
-        student: row.studentId,
-        course: crs.id,
-        term: crs.term,
-        hours: crs.hours,
-        note: crs.hours > 30 ? generateNumber(6, 15) : generateNumber(6, 20),
-      }).save();
-    });
-  });
-
-  res.json(result[0]);
-
-  // const his = await CourseHistory.build({});
-});
-router.get("/generateCourses", [], async (req: Request, res: Response) => {
-  const listeSpec = await Specialite.find();
-
-  //TERM 01
-
-  listeSpec.map(async (spec) => {
-    for (let index = 100; index < 108; index++) {
-      await Course.build({
-        code: spec.name.substring(0, 3).toUpperCase() + index,
-        optional: index > 101 ? true : false,
-        specialite: spec.name,
-        term: 1,
-        hours: generateNumber(20, 40),
-      }).save();
-    }
-  });
-  //TERM 02
-
-  listeSpec.map(async (spec) => {
-    for (let index = 200; index < 208; index++) {
-      await Course.build({
-        code: spec.name.substring(0, 3).toUpperCase() + index,
-        optional: index > 201 ? true : false,
-        specialite: spec.name,
-        term: 2,
-        hours: generateNumber(20, 40),
-      }).save();
-    }
-  });
-  //TERM 03
-
-  listeSpec.map(async (spec) => {
-    for (let index = 300; index < 308; index++) {
-      await Course.build({
-        code: spec.name.substring(0, 3).toUpperCase() + index,
-        optional: index > 301 ? true : false,
-        specialite: spec.name,
-        term: 3,
-        hours: generateNumber(20, 40),
-      }).save();
-    }
-  });
-  //TERM 04
-
-  listeSpec.map(async (spec) => {
-    for (let index = 400; index < 408; index++) {
-      await Course.build({
-        code: spec.name.substring(0, 3).toUpperCase() + index,
-        optional: index > 401 ? true : false,
-        specialite: spec.name,
-        term: 4,
-        hours: generateNumber(20, 40),
-      }).save();
-    }
-  });
-
-  const allcrs = await Course.find();
-
-  res.json(allcrs);
+  const result = await Skill.find();
+  res.json(result);
 });
 
-router.get("/generateUsers", [], async (req: Request, res: Response) => {
-  console.log(`here`);
-  const listeSpec = await Specialite.find();
-  listeSpec.map(async (spec) => {
-    for (let index = 0; index < 80; index++) {
-      let genDate = new Date().getTime();
-      console.log(`genDate`, genDate);
-      await User.build({
-        email: `user${genDate}@gmail.com`,
-        finished: true,
-        specialite: spec._id,
-        fullName: `USER${genDate}`,
-        password: `${genDate}`,
-        prevCourses: [],
-      }).save();
-    }
+router.get('/generateCourses', [], async (req: Request, res: Response) => {
+  console.log('here1');
+  const skills = await Skill.find();
+  if (skills) {
+    console.log('inside');
+
+    const levelsArray: { code: string; label: string }[] = [
+      { code: 'BG', label: 'Beginner' },
+      { code: 'INTM', label: 'Intermediate' },
+      { code: 'ADV', label: 'Advanced' },
+    ];
+    levelsArray.map((level, levelId) => {
+      console.log(`here 2`);
+      const arr = generateArray(generateNumber(5, 8));
+      console.log(`arr`, arr);
+      arr.map((_, idx) => {
+        console.log(`here 3`);
+        skills.map(async (skill) => {
+          console.log(`here 4`);
+          const rd = new Date().getTime();
+          const result = await Course.build({
+            code: skill.name + level.code + rd,
+            title: skill.name + ' ' + level.label + ' Course N°' + rd,
+            skill: skill._id,
+            hours: generateNumber(100, 150),
+            rating: generateNumber(2, 5),
+            level: levelId + 1,
+          }).save();
+          console.log(`result`, result);
+          await Skill.updateOne(
+            { name: skill.name },
+            { $push: { courses: result.id } }
+          );
+        });
+      });
+    });
+  }
+  const resultCourses = await Course.find();
+  res.json(resultCourses);
+});
+
+router.get('/generateFormations', [], async (req: Request, res: Response) => {
+  formationsData.map(async (formation) => {
+    const resultFormation = await Formation.build({
+      title: formation.title,
+      courses: [],
+      image: '',
+    }).save();
+    await formation.courses.map(
+      async ({ skill, prerequisite, aquiredLevel }) => {
+        const sk = await Skill.findOne({ name: `${skill}` });
+        const result = await FormationCourse.build({
+          skill: sk?._id,
+          prerequisite,
+          aquiredLevel,
+          formation: resultFormation._id,
+        })
+          .save()
+          .then(async (result) => {
+            await Formation.findByIdAndUpdate(resultFormation.id, {
+              $push: { courses: result },
+            });
+            return result;
+          });
+      }
+    );
+    console.log('3');
   });
-  res.json("done");
+  console.log('4');
+  const fms = await Formation.find();
+  res.json(fms);
 });
 
 export { router as GeneratorRouter };
